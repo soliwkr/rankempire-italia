ad8a5c459478be16333fe2b5d2ab2de2fbb6c241
Walker White <soliwkr@gmail.com>
2026-04-27 06:19:16 +0200

    feat(04-01): create migration 0004_add_deploy_columns.sql for D1
    - 3 ALTER TABLE statements in Drizzle Kit format
    - Adds github_repo_url, pages_project_name, pages_url columns to projects
    - Uses statement-breakpoint separator between statements

---
+++ factory-core/migrations/0004_add_deploy_columns.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `projects` ADD `github_repo_url` text;--> statement-breakpoint
+ALTER TABLE `projects` ADD `pages_project_name` text;--> statement-breakpoint
+ALTER TABLE `projects` ADD `pages_url` text;
